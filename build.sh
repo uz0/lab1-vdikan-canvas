a450644b0703e5dbff895c12a77f38f9550835d0
Vladimir Dikan <vdikan@vivaldi.net>
2022-07-28 01:02:39 +0200

    contract implementation attempt

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 mkdir -p out
 mkdir -p neardev
-cp target/wasm32-unknown-unknown/release/game.wasm ./out/main.wasm
+cp target/wasm32-unknown-unknown/release/lab1_vdikan_canvas.wasm ./out/main.wasm
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{env, near_bindgen};
+use std::time::SystemTime;
 // use near_sdk::collections::{Vector};
-// use near_sdk::json_types::Base64VecU8;
+// use near_sdk::json_types::Base64&VecU8;
 // use near_sdk::serde::{Deserialize, Serialize};
 // use near_sdk::{
 //     env, near_bindgen, BlockHeight, BorshStorageKey, PanicOnDefault,
@@ -11,35 +12,39 @@ use near_sdk::{env, near_bindgen};
 
 // near_sdk::setup_alloc!();
 
-// RULES:
-// empty block -> has 3 neighboring blocks-> live
-// live block -> has 2 neighboring blocks -> live
-// empty|live -> empty
-
-// ....
-// .X..
-// .XX.
-// ....
-
-// u8 = ........
-
 const WIDTH: usize = 16;
 const HEIGHT: usize = 16;
 
-// const FIELD_LEN: usize = (WIDTH / 8) * HEIGHT;
+/// Flatten (x, y) coordinates into an index of a 1-dimensional vector.
+// /// Also checks for the limits ant appplies -1 offset.
+pub fn coords_to_index(x: usize, y: usize) -> usize {
+    (x - 1) * HEIGHT + (y - 1)
+}
 
+pub fn get_stime_as_nanos() -> i64 {
+    let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH);
+    let now_ns = match now {
+        Ok(n) => n.as_nanos() as i64,
+        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    };
+    now_ns
+}
+
+/// Structure of a single pixel that form a `Canvas`.
 #[derive(Clone, BorshDeserialize, BorshSerialize)]
 pub struct CanvasPixel {
     pub color: [u8; 3],
     pub release_timestamp: u64,
 }
 
+/// `Canvas` contract state definition.
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct Canvas {
     pub field: Vec<CanvasPixel>, // look upon near's Vector here
 }
 
+/// `Canvas` contract function implementations.
 #[near_bindgen]
 impl Canvas {
     #[init]
@@ -54,8 +59,87 @@ impl Canvas {
             ],
         }
     }
+
+    fn get_pixel(&self, x: usize, y: usize) -> &CanvasPixel {
+        let pixel = match self.field.get(coords_to_index(x, y)) {
+            Some(p) => p,
+            None => {
+                panic!(
+                    "Requested off-limits coordinates {} {}. Canvas limits: ({},{}).",
+                    x, y, WIDTH, HEIGHT
+                )
+            }
+        };
+        pixel
+    }
+
+    pub fn get_pixel_info(&self, x: usize, y: usize) -> String {
+        // let pixel = self.get_pixel(x, y);
+        let pixel = match self.field.get(coords_to_index(x, y)) {
+            Some(p) => p,
+            None => {
+                panic!(
+                    "Requested off-limits coordinates {} {}. Canvas limits: ({},{}).",
+                    x, y, WIDTH, HEIGHT
+                )
+            }
+        };
+
+        let release_time = pixel.release_timestamp as i64;
+        let system_time = get_stime_as_nanos();
+        let time_diff = release_time - system_time;
+
+        let timereport = match time_diff {
+            td if td <= 0 => String::from("is free, one can paint it now"),
+            td if td > 0 => {
+                format!("is held, released in {} seconds", time_diff / 1_000_000_000)
+            }
+            _ => unreachable!(),
+        };
+
+        format!(
+            "Pixel({},{}): color({:?}); status: {}.",
+            x, y, pixel.color, timereport,
+        )
+    }
+
+    pub fn set_pixel(&mut self, x: usize, y: usize, color: [u8; 3]) {
+        let index = coords_to_index(x, y);
+        let pixel = match self.field.get(index) {
+            Some(p) => p,
+            None => {
+                panic!(
+                    "Requested off-limits coordinates {} {}. Canvas limits: ({},{}).",
+                    x, y, WIDTH, HEIGHT
+                )
+            }
+        };
+
+        let release_time = pixel.release_timestamp as i64;
+        let system_time = get_stime_as_nanos();
+        let time_diff = release_time - system_time;
+
+        match time_diff {
+            td if td <= 0 => {
+                self.field[index] = CanvasPixel {
+                    color,
+                    release_timestamp: env::block_timestamp() + 5_000_000_000,
+                }
+            }
+            td if td > 0 => {
+                panic!(
+                    "Pixel({},{}) is held, released in {} seconds",
+                    x,
+                    y,
+                    (time_diff / 1_000_000_000)
+                )
+            }
+            _ => unreachable!(),
+        };
+    }
 }
 
+/// Tests for `Canvas` contract.
 #[cfg(not(target_arch = "wasm32"))]
 #[cfg(test)]
 mod tests {
@@ -70,15 +154,56 @@ mod tests {
     }
 
     #[test]
+    fn index_for_coords_within_limits() {
+        assert_eq!(coords_to_index(2, 3), 18);
+        assert_eq!(coords_to_index(5, 5), 68);
+        assert_eq!(coords_to_index(1, 1), 0);
+        assert_eq!(coords_to_index(16, 16), 255);
+    }
+
+    #[test]
+    #[should_panic]
+    fn coords_off_limits_should_panic() {
+        let _i = coords_to_index(0, 1);
+    }
+
+    #[test]
     fn test_new() {
         let context = get_context(false);
         testing_env!(context);
-        let _canvas = Canvas::new();
+        let canvas = Canvas::new();
 
-        println!("{}", _canvas.field[0].release_timestamp);
-        println!("{}", _canvas.field[0].color[0]);
-        println!("{}", _canvas.field[0].color[1]);
-        println!("{}", _canvas.field[0].color[2]);
+        println!("{}", canvas.field[0].release_timestamp);
+        println!("{}", canvas.field[0].color[0]);
+        println!("{}", canvas.field[0].color[1]);
+        println!("{}", canvas.field[0].color[2]);
+    }
+
+    #[test]
+    fn test_get_pixel_info() {
+        let context = get_context(false);
+        testing_env!(context);
+        let canvas = Canvas::new();
+
+        let x: usize = 2;
+        let y: usize = 3;
+        println!("{:?}", canvas.get_pixel_info(x, y));
+    }
+
+    #[test]
+    fn test_set_pixel() {
+        let context = get_context(false);
+        testing_env!(context);
+        let mut canvas = Canvas::new();
+
+        let x: usize = 3;
+        let y: usize = 4;
+        let color = [10u8, 20u8, 30u8];
+
+        canvas.set_pixel(x, y, color);
+        println!("{:?}", canvas.get_pixel_info(x, y));
+        let p = canvas.get_pixel(x, y);
+        println!("{}", p.release_timestamp);
     }
 
     #[test]
